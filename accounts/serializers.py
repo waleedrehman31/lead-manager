dc8197aa4fcc98f8bc368121908fd056dcae5409
Waleed ur Rehman <waleedrehman31@gmail.com>
2021-07-29 21:13:05 +0500

    Django Authentication

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -28,3 +28,14 @@ class RegisterSerializer(serializers.ModelSerializer):
         return user
 
 # login serializer
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+    def validate(self, data):
+        user = authenticate(**data)
+        if user and user.is_active:
+            return user
+        raise serializers.ValidationError("Invalid Credentials")
